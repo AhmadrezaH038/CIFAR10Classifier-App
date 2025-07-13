d652d96029f7936caec6f03ad115530ef7b4a7e3
ahmadreza90hasani3831@gmail.com <ahmadreza90hasani3831@gmail.com>
2025-07-13 13:41:30 +0330

    frontend: landing page written

--- ImageClassifier/ImageClassifier/settings.py
+++ ImageClassifier/ImageClassifier/settings.py
@@ -39,13 +39,22 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'djangorestframework',
 
-    # external apps :
+    # external apps
     'api',
-    'authentication',
+    'authentication.apps.AuthenticationConfig',
     'classifier',
     'frontend',
-    'static',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 
 MIDDLEWARE = [
@@ -124,7 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [ BASE_DIR / 'frontend/static' ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
