2c6fe8b0addd953a5631aa3a8892defac496f214
ahmadreza90hasani3831@gmail.com <ahmadreza90hasani3831@gmail.com>
2025-07-14 17:04:41 +0330

    classifier: endpoints defined for classifying images

--- ImageClassifier/classifier/views.py
+++ ImageClassifier/classifier/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 from django.shortcuts import render
+from .inferences import classify_image
 
-# Create your views here.
+@login_required
+@require_http_methods(['GET', 'POST'])
+def run_classifier(request):
+    result = None
+    if request.method == "POST" and request.FILES.get('image'):
+        result = classify_image(request.user, request.FILES['image'])
+    return render(request, '', {"result": result})
