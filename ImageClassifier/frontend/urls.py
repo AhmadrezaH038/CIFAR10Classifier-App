905bcc691562513d35a807a79a37cdc81f816239
ahmadreza90hasani3831@gmail.com <ahmadreza90hasani3831@gmail.com>
2025-07-24 13:41:53 +0330

    frontend: bro

--- ImageClassifier/frontend/urls.py
+++ ImageClassifier/frontend/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.home_view, name='home')
-]
+app_name = 'frontend'
 
+urlpatterns = [
+    path('', views.classifier_page, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard')
+]
--- ImageClassifier/frontend/views.py
+++ ImageClassifier/frontend/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from classifier.models import RecentAction
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 
-def home_view(request):
-    return render(request, 'frontend/')
+@login_required
+def classifier_page(request):
+    return render(request, 'base.html')
+
+
+@login_required
+def dashboard(request):
+    actions = (
+        RecentAction.objects.filter(user=request.user).order_by('-timestamp')[:10]
+    )
+    return render(request, 'dashboard.html', {'actions': actions})
